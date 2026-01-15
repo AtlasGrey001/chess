2f9cd3f1f7a3316a130cfdd6028bcbd66f365b9b
Cameron Kersey <cadeke@byu.edu>
2026-01-15 14:06:21 -0800

    Update ChessPosition.java

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,8 +7,12 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
-
+    public int piece_col;
+    public int piece_row;
+    
     public ChessPosition(int row, int col) {
+        piece_col = col;
+        piece_row = row;
     }
 
     /**
@@ -16,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return piece_row;
     }
 
     /**
@@ -24,6 +28,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return piece_col;
     }
 }
