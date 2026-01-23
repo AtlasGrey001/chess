6b257abc5799c3ad69c094b767f9a9c76a501a90
Cameron Kersey <cadeke@byu.edu>
2026-01-22 19:43:42 -0800

    Update ChessPiece.java

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-//not
+//done?
 public class ChessPiece {
 
     private ChessGame.TeamColor piece_color;
@@ -76,12 +76,7 @@ public class ChessPiece {
         }
     }
 
-    /**
-     *
-     * @param board
-     * @param myPosition
-     * @return
-     */
+    //the king piece
     public Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int[][] move_tos = {{0,1},{0,-1},{1,0},{1,1},{1,-1},{-1,0},{-1,1},{-1,-1}};
@@ -98,12 +93,7 @@ public class ChessPiece {
         return good_moves;
     }
 
-    /**
-     *
-     * @param board
-     * @param myPosition
-     * @return
-     */
+    //the queen
     public Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int[][] move_tos = {{0,1},{0,-1},{1,0},{1,1},{1,-1},{-1,0},{-1,1},{-1,-1}};
@@ -111,6 +101,7 @@ public class ChessPiece {
         return good_moves;
     }
 
+    //the bishop
     public Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int[][] move_tos = {{1,1},{1,-1},{-1,1},{-1,-1}};
@@ -118,6 +109,7 @@ public class ChessPiece {
         return good_moves;
     }
 
+    //the knight
     public Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int[][] move_tos = {{1,2},{1,-2},{2,1},{2,-1},{-1,2},{-1,-2},{-2,1},{-2,-1}};
@@ -134,6 +126,7 @@ public class ChessPiece {
         return good_moves;
     }
 
+    //the rook
     public Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int[][] move_tos = {{0,1},{0,-1},{1,0},{-1,0}};
@@ -141,35 +134,74 @@ public class ChessPiece {
         return good_moves;
     }
 
+    //the pawn
     public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> good_moves = new ArrayList<>();
         int y = myPosition.getRow();
         int x = myPosition.getColumn();
+        int move_to;
+        int start_row;
+        int promo_row;
+
         if (this.piece_color == ChessGame.TeamColor.WHITE){
-            int move_to = 1;
-            int start_row = 2;
-            int promo_row = 8;
+            move_to = 1;
+            start_row = 2;
+            promo_row = 8;
         }
         else{
-            int move_to = -1;
-            int start_row = 7;
-            int promo_row = 1;
+            move_to = -1;
+            start_row = 7;
+            promo_row = 1;
         }
 
         //one jump forward
-        int move_one = y + move_to;
-        if (move_one > 0 && move_one < 9){
-            ChessPosition one_jump = new ChessPosition(move_one, x);
+        int one_row = y + move_to;
+        if (one_row > 0 && one_row < 9){
+            ChessPosition one_jump = new ChessPosition(one_row, x);
             if (board.getPiece(one_jump) == null){
-                if (move_one == promo_row){
-                    //promotion
+                //promotion
+                if (one_row == promo_row){
+                    good_moves.add(new ChessMove(myPosition, one_jump, PieceType.QUEEN));
+                    good_moves.add(new ChessMove(myPosition, one_jump, PieceType.BISHOP));
+                    good_moves.add(new ChessMove(myPosition, one_jump, PieceType.KNIGHT));
+                    good_moves.add(new ChessMove(myPosition, one_jump, PieceType.ROOK));
                 }
                 else{good_moves.add(new ChessMove(myPosition, one_jump, null));}
             }
         }
 
-        //two jump forward
-        if ()
+        //two jump forward, from start only
+        int two_row = y + (move_to * 2);
+        if (y == start_row && board.getPiece(new ChessPosition(one_row, x)) == null){
+            if (two_row > 0 && two_row < 9){
+                ChessPosition two_jump = new ChessPosition(two_row, x);
+                if (board.getPiece(two_jump) == null){
+                    good_moves.add(new ChessMove(myPosition, two_jump, null));
+                }
+            }
+        }
+
+        //capture enemy piece, and promo possible...
+        int[][] enemy_pos = {{move_to,1},{move_to,-1}};
+        for (int[] enemy : enemy_pos){
+            int y_new = y + enemy[0];
+            int x_new = x + enemy[1];
+            if ((y_new < 1 || y_new > 8 || x_new < 1 || x_new > 8) == false){
+                ChessPosition new_pos = new ChessPosition(y_new, x_new);
+                ChessPiece square = board.getPiece(new_pos);
+
+                if (square != null && square.getTeamColor() != this.piece_color){
+                    if (y_new == promo_row){
+                        good_moves.add(new ChessMove(myPosition, new_pos, PieceType.QUEEN));
+                        good_moves.add(new ChessMove(myPosition, new_pos, PieceType.BISHOP));
+                        good_moves.add(new ChessMove(myPosition, new_pos, PieceType.KNIGHT));
+                        good_moves.add(new ChessMove(myPosition, new_pos, PieceType.ROOK));
+                    }
+                    else {good_moves.add(new ChessMove(myPosition, new_pos, null));}
+                }
+            }
+        }
+        return good_moves;
     }
 
     /**
@@ -180,7 +212,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        //throw new RuntimeException("Not implemented");
         Collection<ChessMove> my_moves = new ArrayList<>();
         switch (piece_type){
             case KING:
