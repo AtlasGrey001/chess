a4400af99e563497d9a8062b61ee217e1a7f1e9a
Cameron Kersey <cadeke@byu.edu>
2026-01-23 19:03:09 -0800

    Update ChessBoard.java

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -62,26 +62,23 @@ public class ChessBoard {
             addPiece(new ChessPosition(2, x), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
             addPiece(new ChessPosition(7, x), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
         }
-        //rook
-        addPiece(new ChessPosition(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
-        addPiece(new ChessPosition(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
-        addPiece(new ChessPosition(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
-        addPiece(new ChessPosition(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
-        //knight
-        addPiece(new ChessPosition(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
-        //bishop
-        addPiece(new ChessPosition(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
-        //royals
-        addPiece(new ChessPosition(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
-        addPiece(new ChessPosition(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
-        addPiece(new ChessPosition(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
-        addPiece(new ChessPosition(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+
+        ChessPiece.PieceType[] back_row = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+
+        //rook - royals
+        for (int x = 1; x <= 8; x++){
+            addPiece(new ChessPosition(1,x), new ChessPiece(ChessGame.TeamColor.WHITE, back_row[x-1]));
+            addPiece(new ChessPosition(8,x), new ChessPiece(ChessGame.TeamColor.BLACK, back_row[x-1]));
+        }
     }
 
     //rewrite
@@ -90,8 +87,8 @@ public class ChessBoard {
         if (this == my_obj){return true;}
         if (my_obj == null || getClass() != my_obj.getClass()){return false;}
         ChessBoard new_obj = (ChessBoard) my_obj;
-        for (int y = 1; y < 9; y++){
-            for (int x = 1; x < 9; x++){
+        for (int y = 0; y < 8; y++){
+            for (int x = 0; x < 8; x++){
                 ChessPiece piece_one = this.game_board[y][x];
                 ChessPiece piece_two = new_obj.game_board[y][x];
                 if (piece_one == null && piece_two == null){continue;}
@@ -107,8 +104,8 @@ public class ChessBoard {
     public int hashCode(){
         //we don't want 31 * 0 bc that = 0
         int answer = 1;
-        for (int y = 1; y < 9; y++){
-            for (int x = 1; x < 9; x++){
+        for (int y = 0; y < 8; y++){
+            for (int x = 0; x < 8; x++){
                 ChessPiece piece = game_board[y][x];
                 int value = 0;
                 if (piece != null){value = piece.hashCode();}
