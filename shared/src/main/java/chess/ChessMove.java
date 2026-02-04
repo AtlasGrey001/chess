899ae66900fe431c2ca81ce4c27fabba17113aac
Cameron Kersey <cadeke@byu.edu>
2026-02-04 14:37:54 -0800

    Update ChessMove.java

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -6,31 +6,30 @@ package chess;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-//done
 public class ChessMove {
-    private ChessPosition start_pos;
-    private ChessPosition end_pos;
-    private ChessPiece.PieceType promo_piece;
+    private ChessPosition spos;
+    private ChessPosition epos;
+    private ChessPiece.PieceType promo;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
-        start_pos = startPosition;
-        end_pos = endPosition;
-        promo_piece = promotionPiece;
+        this.spos=startPosition;
+        this.epos=endPosition;
+        this.promo=promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        return start_pos;
+        return spos;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        return end_pos;
+        return epos;
     }
 
     /**
@@ -39,30 +38,27 @@ public class ChessMove {
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
-    public ChessPiece.PieceType getPromotionPiece() {return promo_piece;}
-
-    //rewrite
-    @Override
-    public boolean equals(Object my_obj){
-        if (this == my_obj){return true;}
-        if (my_obj == null || getClass() != my_obj.getClass()){return false;}
-        ChessMove new_obj = (ChessMove) my_obj;
-        if (!start_pos.equals(new_obj.start_pos)){return false;}
-        if (!end_pos.equals(new_obj.end_pos)){return false;}
-        if (promo_piece == null && new_obj.promo_piece == null){return true;}
-        if (promo_piece == null || new_obj.promo_piece == null){return false;}
-        boolean answer = (promo_piece == new_obj.promo_piece);
-        return answer;
+    public ChessPiece.PieceType getPromotionPiece() {
+        return promo;
     }
 
-    //rewrite
+    @Override
+    public boolean equals(Object o){
+        if(this==o)return true;
+        if(o==null || getClass() != o.getClass())return false;
+        ChessMove no = (ChessMove) o;
+        if(!spos.equals(no.spos))return false;
+        if(!epos.equals(no.epos))return false;
+        if(promo==null && no.promo==null)return true;
+        if(promo==null || no.promo==null)return false;
+        return promo==no.promo;
+    }
+
     @Override
     public int hashCode(){
-        int answer = start_pos.hashCode();
-        answer = (31 * answer + end_pos.hashCode());
-        int value = 0;
-        if (promo_piece != null){value = promo_piece.hashCode();}
-        answer = (31 * answer + value);
-        return answer;
+        int ans = 31*spos.hashCode()+epos.hashCode();
+        int val=0;
+        if(promo!=null)val=promo.hashCode();
+        return 31*ans+val;
     }
 }
