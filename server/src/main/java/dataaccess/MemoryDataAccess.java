f9b29a270d3969ad9e0414d752dfc00c73940e69
Cameron Kersey <cadeke@byu.edu>
2026-03-01 23:46:27 -0800

    Update MemoryDataAccess.java

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,27 +1,35 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class MemoryDataAccess implements DataAccess {
     private final Map<String,UserData> users=new HashMap<>();
     private final Map<String,AuthData> authTokens=new HashMap<>();
     private final Map<Integer,GameData> games=new HashMap<>();
+    private final Map<Integer, ChessGame> engines=new HashMap<>();
     private int nextGameID=1;
+    private final Gson gson=new Gson();
 
     @Override
     public void clear() {
         users.clear();
         authTokens.clear();
         games.clear();
+        engines.clear();
         nextGameID=1;
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-        if (users.containsKey(user.username())) {throw new DataAccessException("User already exists");}
+        if (users.containsKey(user.username())) throw new DataAccessException("User already exists");
         users.put(user.username(),user);
     }
 
@@ -46,11 +54,12 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public GameData createGame(String gameName) throws DataAccessException {
+    public GameData createGame(String gameName) {
         int id=nextGameID++;
-        GameData game=new GameData(id,gameName,null,null);
-        games.put(id,game);
-        return game;
+        GameData data=new GameData(id,gameName,null,null,false);
+        games.put(id, data);
+        engines.put(id,new ChessGame());
+        return data;
     }
 
     @Override
@@ -65,7 +74,22 @@ public class MemoryDataAccess implements DataAccess {
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
-        if (!games.containsKey(game.game_ID())) {throw new DataAccessException("Game does not exist");}
-        games.put(game.game_ID(),game);
+        if (!games.containsKey(game.gameID())) throw new DataAccessException("Game does not exist");
+        games.put(game.gameID(),game);
+    }
+
+    @Override
+    public void updateEngine(int gameID,ChessGame engine) throws DataAccessException {
+        try {
+            var path=Path.of("engines",gameID+".json");
+            Files.writeString(path,gson.toJson(engine));
+        } catch (IOException ex) {throw new DataAccessException("Unable to write engine file",ex);}
+    }
+
+    @Override
+    public ChessGame getEngine(int gameID) throws DataAccessException {
+        ChessGame engine=engines.get(gameID);
+        if (engine==null) throw new DataAccessException("Engine does not exist for game "+gameID);
+        return engine;
     }
 }
