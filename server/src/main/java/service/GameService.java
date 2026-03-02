c7ff46388611ad9888319e0c66f4af49af5e95b0
Cameron Kersey <cadeke@byu.edu>
2026-03-02 00:28:07 -0800

    Update GameService.java

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,8 @@
 package service;
 
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.InvalidMoveException;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import model.AuthData;
@@ -9,78 +12,84 @@ import java.util.Collection;
 public class GameService {
     private final DataAccess dataAccess;
 
-    public GameService(DataAccess dataAccess) {
-        this.dataAccess=dataAccess;
-    }
+    public GameService(DataAccess dataAccess) {this.dataAccess=dataAccess;}
 
-    // post game
-    public CreateGameResult createGame(String authToken, CreateGameRequest request)
+    public CreateGameResult createGame(CreateGameRequest request)
             throws DataAccessException, BadRequestException, UnauthorizedException {
-        // check auth
-        var auth=validateAuth(authToken);
-        // check request
-        if (request==null||request.game_name()==null||request.game_name().isBlank()) {
-            throw new BadRequestException("bad request");
-        }
-        // create game
-        var game=dataAccess.createGame(request.game_name());
-        return new CreateGameResult(game.game_ID());
+        var auth=validateAuth(request.authToken());
+        if (request.gameName()==null||request.gameName().isBlank()) throw new BadRequestException("Bad Request");
+        var game=dataAccess.createGame(request.gameName());
+        return new CreateGameResult(game.gameID());
     }
 
-    // get game
     public ListGamesResult listGames(String authToken)
             throws DataAccessException, UnauthorizedException {
-        // check auth
         validateAuth(authToken);
         Collection<GameData> games=dataAccess.listGames();
         return new ListGamesResult(games);
     }
 
-    // set game
-    public void joinGame(String authToken,JoinGameRequest request)
+    public void joinGame(JoinGameRequest request)
             throws DataAccessException, BadRequestException, UnauthorizedException {
-        // check auth
-        var auth=validateAuth(authToken);
-        // check request
-        if (request==null||request.gameID()==null||request.playerColor()==null||request.playerColor().isBlank()) {
-            throw new BadRequestException("bad request");
-        }
-
-        var game=dataAccess.getGame(request.gameID());
-        if (game==null) {
-            throw new BadRequestException("bad request");
-        }
-
-        // check color
-        String color=request.playerColor().toUpperCase();
-        if (!color.equals("WHITE")&&!color.equals("BLACK")) {
-            throw new BadRequestException("bad request");
-        }
-
-        // check if seat taken
+        var auth=validateAuth(request.authToken());
+        if (request.gameID()==null||request.playerColor()==null||request.playerColor().isBlank()) throw new BadRequestException("Bad Request");
+        var game = dataAccess.getGame(request.gameID());
+        if (game==null) throw new BadRequestException("Bad Request");
+        var color=request.playerColor().toUpperCase();
+        if (!color.equals("WHITE")&&!color.equals("BLACK")) throw new BadRequestException("Bad Request");
         if (color.equals("WHITE")) {
-            if (game.white_username()!=null) {
-                throw new AlreadyTakenException("already taken");
-            }
+            if (game.whiteUsername()!=null) throw new AlreadyTakenException("Already Taken");
             dataAccess.updateGame(game.withWhite(auth.username()));
         } else {
-            if (game.black_username()!=null) {
-                throw new AlreadyTakenException("already taken");
-            }
+            if (game.blackUsername()!=null) throw new AlreadyTakenException("Already Taken");
             dataAccess.updateGame(game.withBlack(auth.username()));
         }
     }
 
-    // helper
-    private AuthData validateAuth(String authToken)
+    public AuthData validateAuth(String authToken)
             throws DataAccessException, UnauthorizedException {
-        if (authToken==null||authToken.isBlank()) {
-            throw new UnauthorizedException("unauthorized");
-        }
+        if (authToken==null||authToken.isBlank()) throw new UnauthorizedException("Unauthorized");
         var auth=dataAccess.getAuth(authToken);
-        if (auth==null) {
-            throw new UnauthorizedException("unauthorized");
-        }
+        if (auth==null) throw new UnauthorizedException("Unauthorized");
         return auth;
     }
+
+    public GameData getGameRaw(int gameID) throws DataAccessException {return dataAccess.getGame(gameID);}
+
+    public GameData makeMove(AuthData auth, int gameID, ChessMove move)
+            throws DataAccessException, BadRequestException, InvalidMoveException {
+        var game=dataAccess.getGame(gameID);
+        if (game==null) throw new BadRequestException("Bad Request");
+        if (game.gameOver()) throw new BadRequestException("Game Already Over");
+        var chessGame=dataAccess.getEngine(gameID);
+
+        var turn=chessGame.getTeamTurn();
+        boolean whiteTurn=(turn==ChessGame.TeamColor.WHITE);
+        if (whiteTurn&&!auth.username().equals(game.whiteUsername())) throw new BadRequestException("Not Your Turn");
+        if (!whiteTurn&&!auth.username().equals(game.blackUsername())) throw new BadRequestException("Not Your Turn");
+
+        var legalMoves=chessGame.validMoves(move.getStartPosition());
+        if (legalMoves==null||!legalMoves.contains(move)) throw new BadRequestException("Illegal Move");
+        chessGame.makeMove(move);
+        dataAccess.updateEngine(gameID,chessGame);
+        return game;
+    }
+
+    public GameData resign(AuthData auth,int gameID)
+            throws DataAccessException, BadRequestException {
+        var game=dataAccess.getGame(gameID);
+        if (game==null) throw new BadRequestException("Bad Request");
+        boolean isWhite=auth.username().equals(game.whiteUsername());
+        boolean isBlack=auth.username().equals(game.blackUsername());
+
+        if (!isWhite&&!isBlack) throw new BadRequestException("Not A Player");
+        if (game.gameOver()) throw new BadRequestException("Game Already Over");
+        var updated=game.withGameOver(true);
+        dataAccess.updateGame(updated);
+        return updated;
+    }
+
+    public GameData getGameMetadata(int gameID) throws DataAccessException {return dataAccess.getGame(gameID);}
+
+    public ChessGame getEngine(int gameID) throws DataAccessException {return dataAccess.getEngine(gameID);}
 }
