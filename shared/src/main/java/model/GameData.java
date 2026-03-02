09a3d59fb2a43ea5a8c94a882984c81f50e3f304
Cameron Kersey <cadeke@byu.edu>
2026-03-01 23:38:58 -0800

    Update GameData.java

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,16 +1,17 @@
 package model;
 
-public record GameData(
-        Integer game_ID,
-        String game_name,
-        String white_username,
-        String black_username
-) {
+public record GameData(int gameID,String gameName,String whiteUsername,String blackUsername,boolean gameOver) {
+
     public GameData withWhite(String username) {
-        return new GameData(game_ID,game_name,username,black_username);
+        return new GameData(gameID,gameName,username,blackUsername,gameOver);
     }
 
     public GameData withBlack(String username) {
-        return new GameData(game_ID,game_name,white_username,username);
+        return new GameData(gameID,gameName,whiteUsername,username,gameOver);
     }
+
+    public GameData withGameOver(boolean over) {
+        return new GameData(gameID,gameName,whiteUsername,blackUsername,over);
+    }
+
 }
