f93584d63504e82deb4f9241dcf976c7fd307e3a
Cameron Kersey <cadeke@byu.edu>
2026-03-10 22:43:07 -0700

    Create ErrorResponse.java

---
+++ server/src/main/java/server/ErrorResponse.java
@@ -0,0 +1,9 @@
+package server;
+
+public class ErrorResponse {
+    private final String message;
+
+    public ErrorResponse(String message) {this.message=message;}
+
+    public String getMessage() {return message;}
+}
