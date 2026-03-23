9984feccf00fb25fdc32e1be857fe429a148a696
Cameron Kersey <cadeke@byu.edu>
2026-03-23 15:23:10 -0700

    Create JsonUtil.java

---
+++ client/src/main/java/client/JsonUtil.java
@@ -0,0 +1,12 @@
+package client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtil {
+    private static final Gson gson=new GsonBuilder().create();
+
+    public static <T> T fromJson(String json,Class<T> clazz) {return gson.fromJson(json,clazz);}
+
+    public static String toJson(Object obj) {return gson.toJson(obj);}
+}
