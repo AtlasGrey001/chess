af368d8f702a95c186552960b6c49c1bc9f1f5e3
Cameron Kersey <cadeke@byu.edu>
2026-03-24 15:44:48 -0700

    Update JsonUtil.java

--- client/src/main/java/client/JsonUtil.java
+++ client/src/main/java/client/JsonUtil.java
@@ -4,9 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class JsonUtil {
-    private static final Gson gson=new GsonBuilder().create();
+    private static final Gson GSON=new GsonBuilder().create();
 
-    public static <T> T fromJson(String json,Class<T> clazz) {return gson.fromJson(json,clazz);}
+    public static <T> T fromJson(String json,Class<T> clazz) {return GSON.fromJson(json,clazz);}
 
-    public static String toJson(Object obj) {return gson.toJson(obj);}
+    public static String toJson(Object obj) {return GSON.toJson(obj);}
 }
